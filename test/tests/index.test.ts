164d6daa3d4afa00993a7b5877f6653797d0fee0
tianyong90 <412039588@qq.com>
2018-10-22 01:04:44 +0800

    test

--- test/tests/index.test.ts
+++ test/tests/index.test.ts
@@ -2,6 +2,9 @@ function sum(a: number, b: number) {
   return a + b
 }
 
-test('basic', () => {
-  expect(sum(1, 2)).toBe(3)
+describe('index', () => {
+
+  test('basic', () => {
+    expect(sum(1, 2)).toBe(3)
+  })
 })
